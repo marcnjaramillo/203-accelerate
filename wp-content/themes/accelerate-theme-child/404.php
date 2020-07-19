a4bdf3881fc8511c37e1c41b909fb230acb6f897
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-07-18 21:04:16 -0700

    Start work on 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="404-error" class="site-content">
+		<div class="main-content" role="main">
+      <h1>404</h1>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
