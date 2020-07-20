e07ec1ef0840be6ab00171ea416437bd08abc1c3
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-07-20 00:42:17 -0700

    Add constant for images, new Google font, and secondary widget

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -13,7 +13,8 @@
 // Enqueue scripts and styles
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+  wp_enqueue_style('accelerate-child-google-fonts', '//fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
@@ -31,3 +32,22 @@ function create_custom_post_types() {
   );
 }
 add_action( 'init', 'create_custom_post_types' );
+
+function accelerate_theme_child_widget_init() {
+	
+	register_sidebar( array(
+	    'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
+	    'id' => 'sidebar-2',
+	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	    'after_widget' => '</aside>',
+	    'before_title' => '<h4 class="widget-title">',
+	    'after_title' => '</h4>',
+	) );
+	
+}
+add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
+
+if( !defined(THEME_IMG_PATH)){
+  define( 'THEME_IMG_PATH', get_stylesheet_directory_uri() . '/img' );
+ }
