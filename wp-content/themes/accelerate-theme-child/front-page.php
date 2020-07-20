674e6abd625b799ef57d2d0ec8fe4e5a4af431f8
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-07-20 00:43:07 -0700

    Add Twitter module to homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -58,4 +58,14 @@ get_header(); ?>
     </div>
   </section>
 
+  <section>
+    <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+      <div id="secondary" class="widget-area" role="complementary">
+        <div class="twitter-post">
+          <?php dynamic_sidebar( 'sidebar-2' ); ?>
+        </div>
+      </div>
+    <?php endif; ?>
+  </section>
+
 <?php get_footer(); ?>
